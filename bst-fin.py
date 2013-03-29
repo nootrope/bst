8b011344254601db75fa2a61737bbd59a165f69e
Alberto Gaitán <alberto.gaitan@gmail.com>
2013-03-29 17:35:10 -0400

    Uses a playback duration variable and tests for activity while it increments the playback duration counter.

--- bst-fin.py
+++ bst-fin.py
@@ -11,40 +11,39 @@ port = sys.argv[1]
 
 baseDir = '/home/alberto/cornelius/audio/'
 tracks = ['20121209-205217-edited.wav', '20121210-103310-edited.wav', '20121216-142614-edited.wav', '20121219-145044-edited.wav', '20121220-150925-edited.wav']
-gong = baseDir + 'gong.wav'	# utility notification
+# gong = baseDir + 'gong.wav'	# utility notification
 playThreshold = 10	# visitor goes in dress
-takeABreath = 3	# in seconds
-
-sensorInput = serial.Serial(port, 9600)	# from Arduino
-sensorInput.flushInput()
+playbackDuration = 20	# in seconds
+slept = 0
 
 def readSensor(s):
 	try:
 		inputAsInteger = int(s)
 		if (inputAsInteger > 1023):
-			print '***** Tossing artifact ('+ str(inputAsInteger) + ')'
+			#print '***** Tossing artifact ('+ str(inputAsInteger) + ')'
 			pass
 		else:
-	#		***** DEBUG feedback:
+			# ***** DEBUG feedback:
 			print inputAsInteger
 			return inputAsInteger
 	except ValueError:
 		pass
 
-def playtrack(track):
-	climax = pygame.mixer.Sound(track)
-	sleep(takeABreath)
-	channelA.play(climax)
-	while channelA.get_busy():
-#		if readSensor(sensorInput.readline()) < playThreshold:
-#			channelA.fadeout(1000)
-#			channelA.pause()
-#			channelA.stop()
-#			print '***** below playThreshold'
-#			return False
-#		sleep(0.1)
-		pass
-	return True
+sensorInput = serial.Serial(port, 9600)	# from Arduino
+sensorInput.flushInput()
+
+# def playtrack(track):
+# 	slept = 0
+# 	climax = pygame.mixer.Sound(track)
+# 	channelA.play(climax)
+# 	while channelA.get_busy():
+# 		if (slept < playbackDuration):
+# 			sleep(1)
+# 			slept += 1
+# 			if (slept > playbackDuration):
+# 				channelA.pause()
+# 				return playbackDuration
+# 	return slept
 
 try:
 	pygame.mixer.init(48000, -16, 1, 1024)
@@ -54,7 +53,18 @@ except Exception, e:
 
 while True:
 	if (readSensor(sensorInput.readline()) > playThreshold):
-	 	for bigOh in tracks:
-	 		if not playtrack(baseDir + bigOh):
-	 			#sensorInput.flushInput()
- 				break	# quit-threshold has been met
+	 	for audioFile in tracks:
+			climax = pygame.mixer.Sound(audioFile)
+			channelA.play(climax)
+	 		slept = playbackDuration - slept
+			while channelA.get_busy():
+				if (slept < playbackDuration):
+					sleep(1)
+					slept += 1
+				else:
+					channelA.pause()
+					slept = 0
+					sensorInput.flushInput()
+					while (readSensor(sensorInput.readline()) < playThreshold):
+						pass
+					channelA.unpause()
